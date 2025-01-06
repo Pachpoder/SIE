840a161cd1b3699f20d3735d157cc24994b6df7c
Andres <a.ovando@grupomercafarma.com>
2025-01-06 17:33:07 -0600

    first commit

---
+++ src/api/service.js
@@ -0,0 +1,57 @@
+import simg from '../images/service/1.jpg'
+import simg2 from '../images/service/2.jpg'
+import simg3 from '../images/service/3.jpg'
+import simg4 from '../images/service/4.jpg'
+
+import sImgS1 from '../images/service-single/img-1.jpg'
+import sImgS2 from '../images/service-single/img-2.jpg'
+import sImgS3 from '../images/service-single/img-3.jpg'
+import sImgS4 from '../images/service-single/img-4.jpg'
+
+import sSingleimg1 from '../images/service-single/2.jpg'
+import sSingleimg2 from '../images/service-single/3.jpg'
+
+
+
+
+const Services = [
+    {
+        Id: '1',
+        sImg:simg,
+        sImgS:sImgS1,
+        sTitle: 'Misión',
+        description:'Desarrollar una eficiente gestión, producto de la aplicación de principios, valores bíblicos y empresariales, que nos comprometen con pasión a desarrollar los productos de alta calidad que garanticen la satisfacción de nuestros clientes.',
+        des2:'Lacus, etiam sed est eu tempus need Temer diam congue laoret.',
+        des3:'One way to categorize the activities is in terms of the professional’s area of expertise such as competitive analysis, corporate strategy the activities',
+        icon:'flaticon-advisor',
+        ssImg1:sSingleimg1,
+        ssImg2:sSingleimg2,
+    },
+    {
+        Id: '2',
+        sImg:simg2,
+        sImgS:sImgS2,
+        sTitle: 'Visión',
+        description:'Ser una compañía Farmacéutica próspera para sus socios y colaboradores',
+        des2:'Lacus, etiam sed est eu tempus need Temer diam congue laoret .',
+        des3:'One way to categorize the activities is in terms of the professional’s area of expertise such as competitive analysis, corporate strategy the activities',
+        icon:'flaticon-team',
+        ssImg1:sSingleimg1,
+        ssImg2:sSingleimg2,
+    },
+    {
+        Id: '3',
+        sImg:simg3,
+        sImgS:sImgS3,
+        sTitle: 'Valores',
+        description:'valores',
+        des2:'Lacus, etiam sed est eu tempus need Temer diam congue laoret .',
+        des3:'One way to categorize the activities is in terms of the professional’s area of expertise such as competitive analysis, corporate strategy the activities',
+        icon:'flaticon-taxes',
+        ssImg1:sSingleimg1,
+        ssImg2:sSingleimg2,
+    },
+ 
+]    
+
+export default Services;
---
+++ src/components/Extensiones/Extensiones.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@material-ui/core"
+
+
+export const Extensiones = () => {
+  return (
+    <>
+        <Button>Extensiones</Button>
+    </>
+  )
+}
